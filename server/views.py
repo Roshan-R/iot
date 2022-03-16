48bb64a5bdac0cb04af35dbf60c50059aa2869af
Roshan R <roshanr2001@gmail.com>
2022-03-16 18:19:44 +0530

    chore : initial heroku commit

--- server/views.py
+++ server/views.py
@@ -12,7 +12,9 @@ class ReadingViews(APIView):
             return JsonResponse({"status": "success", "data": serializer.data}, status=status.HTTP_200_OK)
         else:
             return JsonResponse({"status": "error", "data": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+
     def get(self, request, id=None):
+        # Don't really know why if condition exists
         if id:
             item = Reading.objects.get(id=id)
             serializer = ReadingSerializer(item)
